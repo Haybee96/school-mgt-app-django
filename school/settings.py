4b58aa09cd99a91e9a916881dc4f0f2d7bf18a17
Haybee96 <abeebayinla@gmail.com>
2020-07-18 18:44:52 +0100

    added allowed hosts

--- school/settings.py
+++ school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n^h%0^#1%i)fgwq&tjti^+mrm_zc^+2f#jj@f03=9dtf_ju0jh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com/sms']
 
 
 # Application definition
