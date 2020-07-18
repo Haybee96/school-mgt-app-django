c8b8d3ae3d585ac6a265a617d5fcbbf2592dc622
Haybee96 <abeebayinla@gmail.com>
2020-07-18 19:37:07 +0100

    added allowed hosts

--- school/urls.py
+++ school/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('registered-courses/', include('courses.urls')),
     path('accounts/login', include('accounts.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += sstatic(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
