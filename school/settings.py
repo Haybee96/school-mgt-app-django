eea00ecfce468d0d0ae259cb6620c74226da529f
Haybee96 <abeebayinla@gmail.com>
2020-07-18 18:21:18 +0100

    added staticfiles

--- school/settings.py
+++ school/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'school/static')
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+# 	os.path.join(BASE_DIR, 'school/static')
+# ]
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 # Message configuration
 from django.contrib.messages import constants as messages
